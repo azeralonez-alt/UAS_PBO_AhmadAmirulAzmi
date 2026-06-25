ea4b24ff9939af60c006e66b8a65540dff4cf626
azeralonez <homeazmiie@gmail.com>
2026-06-25 14:27:13 +0700

    [Tahap 3] Membuat Abstract Class Mahasiswa dan method abstract

--- Mahasiswa.php
+++ Mahasiswa.php
@@ -1,17 +1,15 @@
 <?php
+// File: Mahasiswa.php
 
-// Membuat abstract class bernama Mahasiswa
 abstract class Mahasiswa {
-    
     // Properti/Atribut Terenkapsulasi (protected)
-    // Dipetakan dari kolom database Tahap 1
     protected $id_mahasiswa;
     protected $nama_mahasiswa;
     protected $nim;
     protected $semester;
-    protected $tarifUktNominal; // CamelCase sesuai instruksi soal, memetakan tarif_ukt_nominal dari DB
+    protected $tarifUktNominal;
 
-    // Constructor untuk memetakan nilai dari database secara pas saat objek dibuat
+    // Constructor Induk
     public function __construct($id_mahasiswa, $nama_mahasiswa, $nim, $semester, $tarifUktNominal) {
         $this->id_mahasiswa = $id_mahasiswa;
         $this->nama_mahasiswa = $nama_mahasiswa;
@@ -20,8 +18,7 @@ abstract class Mahasiswa {
         $this->tarifUktNominal = $tarifUktNominal;
     }
 
-    // Metode Abstrak (Tanpa Isi/Body)
-    // Wajib diimplementasikan oleh setiap class anak (Mandiri, Bidikmisi, Prestasi)
+    // Metode Abstrak (Wajib di-overriding oleh kelas anak)
     abstract public function hitungTagihanSemester();
     abstract public function tampilkanSpesifikasiAkademik();
 }
